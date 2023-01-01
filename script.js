259d002964783dcb9a71405fa62ebcf8c1143493
khiciste <khiciste@penguin>
2023-01-01 18:07:31 -0500

    commit 6

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
     // initialize sidenav for mobile
     $('.sidenav').sidenav();
+
+    // slide from right
+    $('.sidenav').sidenav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
   });
