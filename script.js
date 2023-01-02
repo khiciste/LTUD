ffd11e12c32f285bddb147e6566cac79536048b7
khiciste <khiciste@penguin>
2023-01-02 18:23:06 -0500

    commit 10

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
-  });
+
+    // show color icon on hover
+    $('.social-link').hover(function() {
+      $(this).children().toggleClass('hide');
+    });
+  
+});
