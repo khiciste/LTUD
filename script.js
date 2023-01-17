e92729f99db88316a491ba0bc3cfbdb18cadf7a3
khiciste <khiciste@penguin>
2023-01-16 23:47:42 -0500

    add contact form

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
-    // initialize sidenav for mobile
-    $('.sidenav').sidenav();
+  
+  // initialize sidenav for mobile
+  $('.sidenav').sidenav();
 
-    // slide from right
-    $('.sidenav').sidenav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
+  // slide from right
+  $('.sidenav').sidenav({
+    menuWidth: 300, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 
-    // show color icon on hover
-    $('.social-link').hover(function() {
-      $(this).children().toggleClass('hide');
-    });
+  // show color icon on hover
+  $('.social-link').hover(function() {
+    $(this).children().toggleClass('hide');
+  });
+
+  // clear form after submission
+  window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
   
 });
